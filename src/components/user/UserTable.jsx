bd1f94d9d3a7ff11e7ac149c1a44a9463d02daa8
Chamy <yd95069506@naver.com>
2021-10-19 21:30:54 +0900

    :hammer: 사용하지 않는 prop 제거

--- src/components/user/UserTable.jsx
+++ src/components/user/UserTable.jsx
@@ -7,7 +7,7 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 
-function UserTable({ list, error, modalOpen }) {
+function UserTable({ list, modalOpen }) {
   return (
     <TableContainer component={Paper}>
       <Table>
