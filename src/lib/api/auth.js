56c530bb3e1ad52d3bd4259d62b5592b9ce1c10f
Chamy <yd95069506@naver.com>
2021-10-19 15:15:22 +0900

    :sparkles: 로그인 체크 api 추가

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -1,3 +1,4 @@
 import client from './client';
 
 export const login = ({ username, password }) => client.post('/api/admin/login', { username, password });
+export const check = () => client.get('/api/admin/check');
