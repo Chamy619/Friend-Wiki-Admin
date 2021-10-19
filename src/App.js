c83e7e1007fd314d1fbd84b0a960a14ba5451f24
Chamy <yd95069506@naver.com>
2021-10-19 16:09:29 +0900

    :sparkles: 유저 관리 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import UserPage from './pages/UserPage';
 import { Container } from '@mui/material';
 
 function App() {
   return (
     <Container sx={{ height: '100%', display: 'flex', justifyContent: 'center' }}>
       <Route exact path="/login" component={LoginPage} />
+      <Route path="/user" component={UserPage} />
     </Container>
   );
 }
---
+++ src/pages/UserPage.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/common/Header';
+
+function UserPage() {
+  return (
+    <div>
+      <Header />
+      유저 페이지입니다.
+    </div>
+  );
+}
+
+export default UserPage;
